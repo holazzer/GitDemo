f3b4ecad8a58d1bf78dd1317e6b660873a137255
holazzer <751727648@qq.com>
2020-03-04 22:15:09 +0800

    1

--- example.py
+++ example.py
@@ -3,3 +3,7 @@ import sklearn.datasets as ds
 import sklearn.neural_network as nn
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+
+X,y = ds.make_circles(n_samples=1000,noise=0.05)
+plt.scatter(X[:,0],X[:,1],c=y)
+plt.show()
