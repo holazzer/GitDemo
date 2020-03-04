89be53259599edfff1910bc6f5eaa71d1c91895d
holazzer <751727648@qq.com>
2020-03-04 22:15:40 +0800

    2

--- example.py
+++ example.py
@@ -7,3 +7,10 @@ from sklearn.model_selection import train_test_split
 X,y = ds.make_circles(n_samples=1000,noise=0.05)
 plt.scatter(X[:,0],X[:,1],c=y)
 plt.show()
+
+X_train, X_test, y_train, y_test = train_test_split(X,y)
+
+clf = nn.MLPClassifier(hidden_layer_sizes=(50,20),solver='lbfgs',random_state=1,verbose=1)
+
+clf.fit(X_train,y_train)
+
